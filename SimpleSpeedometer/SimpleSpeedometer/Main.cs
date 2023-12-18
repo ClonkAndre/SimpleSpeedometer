ebe31a63a5edef88eb604fd3b93148f9beab2951
E. S. <39125931+ClonkAndre@users.noreply.github.com>
2023-12-18 21:26:35 +0100

    Version 1.3
    - Made compatible with IV-SDK .NET v1.0.
    - Changed extension for the images from “png” to “dds” to be better optimized for the renderer.
    - System.ArgumentException crash should now be fixed for real.

--- SimpleSpeedometer/SimpleSpeedometer/Main.cs
+++ SimpleSpeedometer/SimpleSpeedometer/Main.cs
@@ -3,16 +3,16 @@ using System.Drawing;
 using System.IO;
 
 using IVSDKDotNet;
-using IVSDKDotNet.Direct3D9;
 using static IVSDKDotNet.Native.Natives;
 
-namespace SimpleSpeedometer {
-    public class Main : Script {
+namespace SimpleSpeedometer
+{
+    public class Main : Script
+    {
 
         #region Variables
-        private D3DGraphics gfx;
-        private D3DResource digitsTexture;
-        private D3DResource pinTexture;
+        private IntPtr digitsTexture;
+        private IntPtr pinTexture;
 
         private bool allowDrawing, disableInMP, playerInCar;
         private int texturesAlpha;
@@ -30,16 +30,12 @@ namespace SimpleSpeedometer {
         {
             Initialized += Main_Initialized;
             Tick += Main_Tick;
+            OnFirstD3D9Frame += Main_OnFirstD3D9Frame;
         }
         #endregion
 
         private void Main_Initialized(object sender, EventArgs e)
         {
-            // Create a new D3DGraphics object for this Script.
-            gfx = new D3DGraphics(this);
-            gfx.OnInit += Gfx_OnInit;
-            gfx.OnDeviceEndScene += Gfx_OnDeviceEndScene;
-
             // Load settings file and get things from the settings file
             if (Settings.Load())
             {
@@ -63,101 +59,6 @@ namespace SimpleSpeedometer {
             }
         }
 
-        private void Gfx_OnInit(IntPtr device)
-        {
-            string digitsFilePath = string.Format("{0}\\{1}\\digits.png", ScriptResourceFolder, skin);
-            string pinFilePath = string.Format("{0}\\{1}\\pin.png", ScriptResourceFolder, skin);
-
-            // Check if both files exists
-            if (!File.Exists(digitsFilePath) || !File.Exists(pinFilePath))
-            {
-                CGame.Console.PrintWarning(string.Format("[SimpleSpeedometer] Please make sure that 'digits.png' and 'pin.png' exists in the SimpleSpeedometer\\{0} folder. Mod will be aborted.", skin));
-                Abort();
-                return;
-            }
-
-            // Create digits texture
-            D3DResult r = gfx.CreateD3D9Texture(digitsFilePath);
-            if (r.Error != null)
-            {
-                CGame.Console.PrintError(string.Format("[SimpleSpeedometer] An error occured while trying to create 'digits' texture! Details: {0}", r.Error.ToString()));
-            }
-            else
-            {
-                digitsTexture = r.DXObject as D3DResource;
-            }
-
-            // Create pin texture
-            r = gfx.CreateD3D9Texture(pinFilePath);
-            if (r.Error != null)
-            {
-                CGame.Console.PrintError(string.Format("[SimpleSpeedometer] An error occured while trying to create 'pin' texture! Details: {0}", r.Error.ToString()));
-            }
-            else
-            {
-                pinTexture = r.DXObject as D3DResource;
-            }
-        }
-        private void Gfx_OnDeviceEndScene(IntPtr device)
-        {
-            // If drawing is not allowed then return out of this method.
-            if (!allowDrawing)
-                return;
-            // If radar is off then return out if this method.
-            if (CMenuManager.Display.RadarMode == 0)
-                return;
-
-            // Get rotation value for the pin based on the current vehicle speed
-            float rot = speed * ((float)Math.PI / 112.5f);
-
-            // Gets the radar rectangle
-            RectangleF rect = CGame.GetRadarRectangle();
-
-            // Fading
-            if (IS_PAUSE_MENU_ACTIVE())
-            {
-                // Force fade textures out for pause menu
-                if (doFading)
-                {
-                    if (!(texturesAlpha <= 0))
-                        texturesAlpha -= fadingOutSpeed;
-                }
-                else
-                {
-                    texturesAlpha = 0;
-                }
-            }
-            else
-            {
-                // Fade textures in only if player is in car
-                if (playerInCar)
-                {
-                    if (doFading)
-                    {
-                        if (!(texturesAlpha >= 255))
-                            texturesAlpha += fadingInSpeed;
-                    }
-                    else
-                    {
-                        texturesAlpha = 255;
-                    }
-                }
-            }
-
-            // Prevent alpha value from being under 0 and above 255
-            if (texturesAlpha < 0)
-                texturesAlpha = 0;
-            if (texturesAlpha > 255)
-                texturesAlpha = 255;
-
-            // Draw digits and pin
-            if (!(texturesAlpha <= 0))
-            {
-                gfx.DrawTexture(digitsTexture,  new RectangleF(rect.X - digitsAndPinOffsetX, rect.Y - digitsAndPinOffsetY, rect.Width + digitsAndPinSizeWidth, rect.Height + digitsAndPinSizeHeight), Color.FromArgb(texturesAlpha, Color.White));
-                gfx.DrawTexture(pinTexture,     new RectangleF(rect.X - digitsAndPinOffsetX, rect.Y - digitsAndPinOffsetY, rect.Width + digitsAndPinSizeWidth, rect.Height + digitsAndPinSizeHeight), rot, Color.FromArgb(texturesAlpha, Color.White));
-            }
-        }
-
         private void Main_Tick(object sender, EventArgs e)
         {
             // If is mp session then reset stuff and return
@@ -168,11 +69,11 @@ namespace SimpleSpeedometer {
             }
 
             // Get current vehicle of player
-            CVehicle veh = CPed.FromPointer(CPlayerInfo.FindPlayerPed()).GetVehicle();
+            IVVehicle veh = IVVehicle.FromUIntPtr(IVPed.FromUIntPtr(IVPlayerInfo.FindThePlayerPed()).GetVehicle());
             if (veh != null)
             {
                 // Get the vehicle handle of the current vehicle the player is in
-                uint handle = CPools.GetVehiclePool().GetIndex(veh.GetUIntPtr());
+                uint handle = IVPools.GetVehiclePool().GetIndex(veh.GetUIntPtr());
 
                 // Get the speed of the vehicle
                 GET_CAR_SPEED((int)handle, out speed);
@@ -193,7 +94,16 @@ namespace SimpleSpeedometer {
                 if (!(texturesAlpha <= 0))
                 {
                     if (doFading)
-                        texturesAlpha -= fadingOutSpeed;
+                    {
+                        int newAlpha = texturesAlpha;
+                        newAlpha -= fadingOutSpeed;
+
+                        // Prevent alpha value from being under 0 and above 255
+                        if (newAlpha < 0)
+                            newAlpha = 0;
+
+                        texturesAlpha = newAlpha;
+                    }
                     else
                         texturesAlpha = 0;
                 }
@@ -206,6 +116,103 @@ namespace SimpleSpeedometer {
                     allowDrawing = false;
                 }
             }
+        }
+
+        private void Main_OnFirstD3D9Frame(object sender, EventArgs e)
+        {
+            ImGuiIV.AddDrawCommand(this, () =>
+            {
+                if (ImGuiIV.BeginCanvas(this, out ImGuiIV_DrawingContext ctx))
+                {
+                    // If drawing is not allowed then return out of this method.
+                    if (!allowDrawing)
+                    {
+                        ImGuiIV.EndCanvas();
+                        return;
+                    }
+                    // If radar is off then return out if this method.
+                    if (IVMenuManager.RadarMode == 0)
+                    {
+                        ImGuiIV.EndCanvas();
+                        return;
+                    }
+
+                    // Get rotation value for the pin based on the current vehicle speed
+                    float rot = speed * ((float)Math.PI / 112.5f);
+
+                    // Gets the radar rectangle
+                    RectangleF rect = IVGame.GetRadarRectangle();
+
+                    // Fading
+                    if (IS_PAUSE_MENU_ACTIVE())
+                    {
+                        // Force fade textures out for pause menu
+                        if (doFading)
+                        {
+                            if (!(texturesAlpha <= 0))
+                                texturesAlpha -= fadingOutSpeed;
+                        }
+                        else
+                        {
+                            texturesAlpha = 0;
+                        }
+                    }
+                    else
+                    {
+                        // Fade textures in only if player is in car
+                        if (playerInCar)
+                        {
+                            if (doFading)
+                            {
+                                if (!(texturesAlpha >= 255))
+                                    texturesAlpha += fadingInSpeed;
+                            }
+                            else
+                            {
+                                texturesAlpha = 255;
+                            }
+                        }
+                    }
+
+                    // Prevent alpha value from being under 0 and above 255
+                    if (texturesAlpha < 0)
+                        texturesAlpha = 0;
+                    if (texturesAlpha > 255)
+                        texturesAlpha = 255;
+
+                    // Draw digits and pin
+                    if (!(texturesAlpha <= 0))
+                    {
+                        ctx.AddImage(digitsTexture, new RectangleF(rect.X - digitsAndPinOffsetX, rect.Y - digitsAndPinOffsetY, rect.Width + digitsAndPinSizeWidth, rect.Height + digitsAndPinSizeHeight), Color.FromArgb(texturesAlpha, Color.White));
+                        ctx.AddImageRotated(pinTexture, new RectangleF(rect.X - digitsAndPinOffsetX, rect.Y - digitsAndPinOffsetY, rect.Width + digitsAndPinSizeWidth, rect.Height + digitsAndPinSizeHeight), rot, Color.FromArgb(texturesAlpha, Color.White));
+                    }
+
+                    ImGuiIV.EndCanvas();
+                }
+            });
+
+            string digitsFilePath = string.Format("{0}\\{1}\\digits.dds", ScriptResourceFolder, skin);
+            string pinFilePath = string.Format("{0}\\{1}\\pin.dds", ScriptResourceFolder, skin);
+
+            // Check if both files exists
+            if (!File.Exists(digitsFilePath) || !File.Exists(pinFilePath))
+            {
+                IVGame.Console.PrintWarning(string.Format("[SimpleSpeedometer] Please make sure that 'digits.dds' and 'pin.dds' exists in the SimpleSpeedometer\\{0} folder. Script will be aborted.", skin));
+                Abort();
+                return;
+            }
+
+            // Create digits texture
+            if (ImGuiIV.CreateTextureFromFile(this, digitsFilePath, out IntPtr ptr, out int width, out int height))
+                digitsTexture = ptr;
+            else
+                IVGame.Console.PrintError("[SimpleSpeedometer] Failed to create 'digits' texture!");
+
+            // Create pin texture
+            if (ImGuiIV.CreateTextureFromFile(this, pinFilePath, out IntPtr ptr2, out int width2, out int height2))
+                pinTexture = ptr2;
+            else
+                IVGame.Console.PrintError("[SimpleSpeedometer] Failed to create 'pin' texture!");
         }
 
     }
